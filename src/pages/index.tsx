dbb69f8783aa9a949698b5e0519afc4cbb954c11
kzuabe <ai3ekzk@gmail.com>
2022-04-24 17:07:17 +0900

    feat: firebaseSDK追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,19 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: 'todolist-89bb8.firebaseapp.com',
+  projectId: 'todolist-89bb8',
+  storageBucket: 'todolist-89bb8.appspot.com',
+  messagingSenderId: '71693804254',
+  appId: '1:71693804254:web:3605987dd8b1178253584f',
+  measurementId: 'G-GVWTC8F2CH',
+};
+
+const app = initializeApp(firebaseConfig);
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
