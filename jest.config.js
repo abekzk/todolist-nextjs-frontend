b3b439fb98fd479e93e3e76c681a3c68d7e64405
kzuabe <ai3ekzk@gmail.com>
2022-08-28 10:56:16 +0900

    refactor: fmt実行

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
@@ -12,7 +12,7 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
-}
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 // module.exports = createJestConfig(customJestConfig)
@@ -20,6 +20,6 @@ const customJestConfig = {
 // 外部パッケージのesmモジュール対応のために少し複雑な書き方をする
 // ref: https://github.com/vercel/next.js/issues/36077
 module.exports = async () => ({
-  ...await createJestConfig(customJestConfig)(),
-  transformIgnorePatterns: ['node_modules/(?!(firebase|@firebase)/)',]
-})
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: ['node_modules/(?!(firebase|@firebase)/)'],
+});
