f131a5291882a66af4e64dbdbcbed4dda6d0717b
kzuabe <ai3ekzk@gmail.com>
2022-08-30 03:51:20 +0900

    test: スナップショットテストを追加

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,26 @@
+import IndexPage from '../src/pages/index';
+import { AuthProvider } from '../src/providers/AuthProvider';
+import { mockServer } from '../src/services/api/mocks/server';
+import { getToken, getUser } from '../src/services/firebase/firebase';
+import { render, screen, waitFor } from '@testing-library/react';
+
+// モック
+jest.mock('../src/services/firebase/firebase');
+const mockGetToken = getToken as jest.MockedFunction<typeof getToken>;
+mockGetToken.mockResolvedValue('testtoken');
+const mockGetUser = getUser as jest.MockedFunction<typeof getUser>;
+mockGetUser.mockResolvedValue({ email: 'test@example.com' });
+
+beforeAll(() => mockServer.listen());
+afterEach(() => mockServer.resetHandlers());
+afterAll(() => mockServer.close());
+
+test('renders IndexPage', async () => {
+  const { container } = render(
+    <AuthProvider>
+      <IndexPage />
+    </AuthProvider>
+  );
+  await waitFor(() => screen.getByText('タスク名1'));
+  expect(container).toMatchSnapshot();
+});
